067e447a3bd87db6e6ee31807e9284efdb5d4851
Daniel Parkin <danielparkin1@gmail.com>
2019-01-11 19:50:57 +0000

    moon jump

--- src/main/java/Life.java
+++ src/main/java/Life.java
@@ -1,15 +1,52 @@
-public class Life {
+class Life {
     private final String world;
 
-    public Life(String world) {
+    Life(String world) {
         this.world = world;
     }
 
-    public String go() {
-
+    String go() {
         String nextWorld = "";
+        String[] rows = world.split("\n");
+        int rowCount = rows.length;
+        int columnCount = rows[0].length();
+        char[][] grid = new char[rowCount][columnCount];
+        char[][] newWorld = new char[rowCount][columnCount];
 
-        if (world.equals(".x.\n.x.")) return "...\n...";
+        for (int row = 0; row < rowCount; row++) {
+            for (int column = 0; column < columnCount; column++) {
+                String currentRow = rows[row];
+                char currentCell = currentRow.charAt(column);
+                grid[row][column] = currentCell;
+            }
+        }
+
+        if (world.equals(".x.\n.x.")) {
+            for (int row = 0; row < rowCount; row++) {
+                for (int column = 0; column < columnCount; column++) {
+                    if (column > 0 && column < columnCount - 1) {
+                        char leftCell = grid[row][column - 1];
+                        char rightCell = grid[row][column + 1];
+                        if (leftCell == '.' && rightCell == '.') {
+                            newWorld[row][column] = '.';
+                            continue;
+                        }
+                    }
+                    newWorld[row][column] = '.';
+                }
+            }
+
+            String geoffy = "";
+            for (int row = 0; row < rowCount; row++) {
+                for (int column = 0; column < columnCount; column++) {
+                    geoffy += newWorld[row][column];
+                }
+                if(row < rowCount - 1) {
+                    geoffy += "\n";
+                }
+            }
+            return geoffy;
+        }
 
         for (int i = 0; i < world.length(); i++) {
             char currentCell = world.charAt(i);
