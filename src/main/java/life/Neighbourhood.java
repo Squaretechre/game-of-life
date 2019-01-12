9b0813935ac2dfce1e9678077be011bb68c9bf3f
Daniel Parkin <danielparkin1@gmail.com>
2019-01-12 11:24:31 +0000

    inline method

--- src/main/java/life/Neighbourhood.java
+++ src/main/java/life/Neighbourhood.java
@@ -10,20 +10,16 @@ public class Neighbourhood {
         this.point = point;
     }
 
-    public Point getPoint() {
-        return point;
-    }
-
     public ArrayList<Point> build() {
         var neighbours = new ArrayList<Point>();
-        var top = new Point(getPoint().x, getPoint().y - 1);
-        var topLeft = new Point(getPoint().x - 1, getPoint().y - 1);
-        var topRight = new Point(getPoint().x + 1, getPoint().y - 1);
-        var left = new Point(getPoint().x - 1, getPoint().y);
-        var right = new Point(getPoint().x + 1, getPoint().y);
-        var bottom = new Point(getPoint().x, getPoint().y + 1);
-        var bottomLeft = new Point(getPoint().x - 1, getPoint().y + 1);
-        var bottomRight = new Point(getPoint().x + 1, getPoint().y + 1);
+        var top = new Point(point.x, point.y - 1);
+        var topLeft = new Point(point.x - 1, point.y - 1);
+        var topRight = new Point(point.x + 1, point.y - 1);
+        var left = new Point(point.x - 1, point.y);
+        var right = new Point(point.x + 1, point.y);
+        var bottom = new Point(point.x, point.y + 1);
+        var bottomLeft = new Point(point.x - 1, point.y + 1);
+        var bottomRight = new Point(point.x + 1, point.y + 1);
 
         neighbours.add(top);
         neighbours.add(topLeft);
