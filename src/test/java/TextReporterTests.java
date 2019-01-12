539510bdf7efdd10c6b681c98016dd4ae7d14312
Daniel Parkin <danielparkin1@gmail.com>
2019-01-12 22:33:46 +0000

    report correct text output after a single generation

--- src/test/java/TextReporterTests.java
+++ src/test/java/TextReporterTests.java
@@ -10,6 +10,31 @@ import static junit.framework.TestCase.assertEquals;
 
 public class TextReporterTests {
     @Test
+    public void reports_correct_cell_output_after_one_generation() throws IOException {
+        Point point = new Point(0, 1);
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(2, 1);
+        Point point3 = new Point(3, 1);
+
+        var cells = new Cells();
+        cells.addAt(point);
+        cells.addAt(point1);
+        cells.addAt(point2);
+        cells.addAt(point3);
+
+        var textReporter = new TextReporter(cells, 4, 4);
+
+        var twoDimensionalSpace = new TwoDimensionalSpace(cells, cells, 4, 4);
+        twoDimensionalSpace.tick();
+
+        Class className = TextReporterTests.class;
+        InputStream inputStream = className.getResourceAsStream("/expected-cells-1.txt");
+        String expectedData = IOUtils.toString(inputStream, Charset.defaultCharset());
+
+        assertEquals(expectedData, textReporter.output());
+    }
+
+    @Test
     public void reports_correct_cell_output_after_two_generations() throws IOException {
         Point point = new Point(0, 1);
         Point point1 = new Point(1, 1);
@@ -29,7 +54,7 @@ public class TextReporterTests {
         twoDimensionalSpace.tick();
 
         Class className = TextReporterTests.class;
-        InputStream inputStream = className.getResourceAsStream("/expected-cells-1.txt");
+        InputStream inputStream = className.getResourceAsStream("/expected-cells-2.txt");
         String expectedData = IOUtils.toString(inputStream, Charset.defaultCharset());
 
         assertEquals(expectedData, textReporter.output());
